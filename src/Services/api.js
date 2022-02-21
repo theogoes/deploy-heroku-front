eb207fff864a05f00cd3d4f94da3d8501d19a11a
theogoes <theogoes@gmail.com>
2022-02-21 13:44:57 -0300

    rework

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : 'https://b-partyf.herokuapp.com/'
+    baseURL : 'https://prototipo-partyf.herokuapp.com/'
 })
 
 export default api
