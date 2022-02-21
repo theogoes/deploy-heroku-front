f1cad5d1c499081ffc6b07877a4377a2cfd2f0cc
theogoes <theogoes@gmail.com>
2022-02-21 14:00:17 -0300

    api route fix

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL : 'https://prototipo-partyf.herokuapp.com/'
+    baseURL : 'https://b-partyf.herokuapp.com/'
 })
 
 export default api
